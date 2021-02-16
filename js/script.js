8d47f5b310774857096d9ff6270d6dd51753d46b
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-16 19:43:37 +0100

    Made the basics of the page

--- js/script.js
+++ js/script.js
@@ -2,27 +2,48 @@ const foodURL = 'http://www.recipepuppy.com/api/?i=&q=&p=3';
 const foodAPI = 'http://www.recipepuppy.com/api/';
 const proxy = 'https://noroffcors.herokuapp.com/';
 const selector = document.querySelector('#ingredientSelector');
+const searchBtn = document.querySelector('#searchBtn');
+const ingredients = document.querySelector('#ingredients');
+const dish = document.querySelector('#dish');
+const foodDiv = document.querySelector('.foodDiv');
 
 selector.onchange = function () {
   console.log(selector.value);
 };
 
 const fullAPI = proxy + foodAPI;
-
-async function getFoodAPI(url) {
-  try {
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    document.querySelector('.alert').innerHTML = showAlertTouser(
-      'An error occured',
-      'danger'
-    );
-  } finally {
-    setTimeout(function () {
-      document.querySelector('.alert').innerHTML = '';
-    }, 3000);
+searchBtn.onclick = function (search) {
+  search.preventDefault();
+  const searchTerm = `${proxy}http://www.recipepuppy.com/api/?i=${ingredients.value}&q=${dish.value}&p=${selector.value}`;
+  console.log(searchTerm);
+  foodDiv.innerHTML = '';
+  async function getFoodAPI(url) {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+      const resultArray = result.results;
+      resultArray.forEach((element) => {
+        console.log(element.title);
+        foodDiv.innerHTML += `
+        <div class="foodDiv__dish">
+            <h2>${element.title}</h2>
+            <p>${element.ingredients}<p>
+            <p>Link to dish: <a href="${element.href}">${element.href}</a> </p>
+            <img src="${element.thumbnail}" alt="${element.title}"/>
+        </div>
+        `;
+      });
+    } catch (error) {
+      document.querySelector('.alert').innerHTML = showAlertTouser(
+        'An error occured',
+        'danger'
+      );
+    } finally {
+      setTimeout(function () {
+        document.querySelector('.alert').innerHTML = '';
+      }, 3000);
+    }
   }
-}
-getFoodAPI(fullAPI);
+  getFoodAPI(searchTerm);
+};
