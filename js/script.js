83f3787b9a4e2336ca3057b60f6c42f0174e9dda
Stian <69841774+StianPost@users.noreply.github.com>
2021-02-16 20:32:30 +0100

    tweaked the code, added information class if no recepies were found

--- js/script.js
+++ js/script.js
@@ -15,25 +15,28 @@ const fullAPI = proxy + foodAPI;
 searchBtn.onclick = function (search) {
   search.preventDefault();
   const searchTerm = `${proxy}http://www.recipepuppy.com/api/?i=${ingredients.value}&q=${dish.value}&p=${selector.value}`;
-  console.log(searchTerm);
   foodDiv.innerHTML = '';
   async function getFoodAPI(url) {
     try {
       const response = await fetch(url);
       const result = await response.json();
-      console.log(result);
       const resultArray = result.results;
       resultArray.forEach((element) => {
-        console.log(element.title);
         foodDiv.innerHTML += `
         <div class="foodDiv__dish">
             <h2>${element.title}</h2>
-            <p>${element.ingredients}<p>
+            <img src="${element.thumbnail}" alt="Image of: ${element.title}"/>
+            <p>Some of the <span>listed</span> ingredients: ${element.ingredients}<p>
             <p>Link to dish: <a href="${element.href}">${element.href}</a> </p>
-            <img src="${element.thumbnail}" alt="${element.title}"/>
         </div>
         `;
       });
+      if (resultArray.length === 0) {
+        document.querySelector('.alert').innerHTML = showAlertTouser(
+          'Sorry, no recepies were found.',
+          'information'
+        );
+      }
     } catch (error) {
       document.querySelector('.alert').innerHTML = showAlertTouser(
         'An error occured',
